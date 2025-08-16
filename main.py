a0d52399fed6ccf65d785c4dd5ed73290050c200
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-08-16 12:14:03 +0530

    blurring only the face portion of an image added

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import mediapipe as mp;
 
 # read image.........................
 
-img = cv2.imread(r"C:\Users\Soumyajit Koley\OneDrive\Documents\face_detection 1.jpg")
+image = cv2.imread(r"C:\Users\Soumyajit Koley\OneDrive\Documents\face_detection 1.jpg")
+
+img = cv2.resize(image,(500,500))
+H, W, _ = img.shape
 
 # detect faces.........................
 
@@ -14,9 +17,25 @@ with mp_face_detection.FaceDetection(model_selection =0,min_detection_confidence
     
     output = face_detection.process(img_rgb)
     
-    for detection in output.detections:
+    if output.detections is not None:
+        for detection in output.detections:
         
-        location_data = detection.location_data
-        bbox = location_data.relative_bounding_box
+            location_data = detection.location_data
+            bbox = location_data.relative_bounding_box
         
-        x1,y1,w,h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+            x1,y1,w,h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+            
+            x1 = int (x1*W)
+            y1 = int (y1*H) # creating proper boundry using coordinate
+            w = int (w*W)
+            h = int (h*H)
+            
+            #img = cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(0,255,0),10)
+            
+            img[x1:x1+w, y1:y1+h, :]= cv2.blur(img[x1:x1+w, y1:y1+h, :],(50,50))
+    
+            cv2.imshow("frame",img)
+            cv2.waitKey(0)
+
+# blur faces....................................
+
